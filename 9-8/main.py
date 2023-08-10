ef9e059144f48ee31679a4fcd0d90aa132ebb1de
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-08-10 14:27:37 +0300

    9-8python

---
+++ 9-8/main.py
@@ -0,0 +1,82 @@
+# print('hello world!')
+# print('hothaifa zoubi')
+# print('hello')
+
+# salary = 12000
+# salary = 9000  # override
+# print(salary)
+
+# data types
+# int - 1 , 0 , -99 , 199 52661801
+# float - 1.1  2.6  0.3  -99.1  11.0
+# str - 'hello' , " " , '0526618010'
+# bool - True , False
+
+# x = 1.5
+# y = 4
+#
+# first_name = 7  # snake case
+# firstName = 11  # camel case
+# FirstName = 11  # pascal case
+# students_count = 2
+# print(type('True'))
+
+# name = 'adi'
+# name2 = name
+# name = 'ameer'
+# print(name2)
+
+# x = 3
+# y = x
+# x = 5
+#
+# print(x)
+# print(y)
+
+
+# a = 10
+# b = 3
+#
+# print(a + b)  # 13 +
+# print(a - b)  # 7 -
+# print(a * b)  # 30 *
+# print(a / b)  # 10 / 3 -> float
+# print(a // b)  # whole division
+# print(a % b)  # modulo
+# print(2 ** 4)  # power 2^4
+#
+# name = 'razi fero'
+#
+# print(name)
+# # print(len(name)) # length
+# # print(name[0]) # first index 0
+# # print(name[-1]) # last index -1
+# # print(name[0:4])  # print the name[start:end]
+# # print(name[0:-5])  # print the name[start:end]
+# # print(name[5:9])  # print the name[start:end]
+# # print(name[5:])  # print the name[start:] to the last of the word
+# # print(name[:5])  # print the name[:end] from the start till the given number
+# # print(name[2:])  # print the name[start:] to the last of the word
+# # print(name[1::2]) # from start to end with 2 lt skip
+# # print(name[::-1]) # reverse the word
+# # print(name[::-1]) # name[start:end:step] - name[end:start:step]
+# print(name[4::-1])  # name[start:end(not includes):step] - name[end:start:step]
+
+# text = 'joey doesnt share food'
+#
+# print(text[::-2])
+
+# x = '\"joey\" \t doesn\'t share \n  food \\true'
+# path = r'c:\\Users\t1\hodi' # row str without escape seq
+# \\ -> \
+# \' -> '
+# \" -> "
+# \n new line
+# \t -> tab 4 spaces - 2 spaces
+
+# print(x)
+
+name = 'tzahi'
+age = 23
+
+print(f'my name is {name} and im {age} years old')  # formatted string
