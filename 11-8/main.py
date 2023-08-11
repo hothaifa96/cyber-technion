1cfad8adee7247e0390fb0af82e0d306cf1451b7
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-08-11 09:54:53 +0300

    newcommit

---
+++ 11-8/main.py
@@ -0,0 +1,73 @@
+# wrapup
+
+# 3 4 66 -99 0   int
+# 3.0 11 -99.11 0.3  float
+# True False  bool
+# 'true' "007"     str
+
+# var_name = var_value
+# name = 'ameer'
+#
+# print('hello ' + name)  # concat
+# print(f'hello {15/3}')  # formatted string
+
+# math opr
+# +
+# -
+# *
+# /  flaot
+# // int
+# %  19%5 = 4  3%5 =
+# ** power
+
+# type() -> return the datatype of an object
+
+# print(type(True))
+
+# stam = None
+# print(type(stam))
+# str to int
+# x = '3'
+# print(type(x))
+# print(x+x)
+# x = int(x)
+#
+# print(type(x))
+# print(x+x)
+
+# y = '3.8'
+#
+# y = float(y)
+#
+# y = int(y)
+#
+# print(type(y))
+# print(y)
+
+# number1 = 55
+# print(number1)
+# number1 = float(number1)
+# print(number1)
+
+# text = 'hello'
+# text = bool(text)
+# print(text)
+
+# number2 = 0
+# number2 = bool(number2)
+# print(number2)
+
+# x = None
+# x = bool(x)
+# print(x)
+
+name = input('please enter your name :')
+
+print(f'hello my name is {name}')
+print(type(name))
+
+# write a python code  to receive an email from the user
+# - print the length of the email (hint len())
+# - print the domain of the email (gmail.com)
+# - print each odd letter in the email using [start:end:step]
+# - **challenge print the email 3 times in the same line
