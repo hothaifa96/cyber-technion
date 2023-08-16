f3d07be1c34abdf05742eb21d201565402e3cfd3
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-08-16 18:36:56 +0300

    mesasge

---
+++ 16-8/homework-exercise1_sol.py
@@ -0,0 +1,58 @@
+# q1
+word = 'cYBerSecUrity1sHere'
+# index = 0
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# print(f'the index {index} is upper {word[index].isupper()}')
+# index += 1
+# -----------------
+# 1.1
+# print(word[0])
+# print(word[-1])
+# # 1.2
+# print(len(word))
+# # 1.3
+# print(word[1], word[2])
+# print(f'{word[1]} {word[2]}')
+# print(word[1:3])
+
+# 1.4
+print(word)
+print(word.capitalize())  # Cybersecurity1shere
+print(word.title())  # Cybersecurity1Shere
+# 1.6
+print(word.replace('e', 'x', 2))  # replace only 2 e's in the word
---
+++ 16-8/homework_variablelab_sol.py
@@ -0,0 +1,62 @@
+# q1
+# name = input('please enter your name:')
+# print(f'hello {name}')
+# print('hello', name)
+import math
+
+# q2
+
+# hours = float(input('hours :'))
+# rate = float(input('rate :'))
+# pay = rate * hours
+# pay = round(pay,2) # floor -> math ceil -> math ,, round
+# print(f'pay: {pay:.2f}')  # print only 2 digits after the decimal point
+# x = 15.666666
+# print(f'{x:.2f}')
+# print(round(x, 2))
+
+
+# q3
+#
+# width = 17
+# height = 12.0
+#
+# print(width / 2, "- the type is", type(width / 2))
+# print(f'{width / 2}  {type(width / 2)} ')  # -> 8.5  float
+# print(height / 3)  # -> 4.0 float
+# print((width + 1) / 2)  # -> 9.0 float
+# print(width * 12)  # 204 int
+# print(width * height)  # -> 204.0 float
+# print(1 + 2 * 5)  # -> 11 int
+
+# q4.1
+
+# # ()   ->   **  -> */ -> +-
+# ans1 = 16 ** 1 / 2
+# print(ans1)
+# ans2 = 16 ** 0.5  # sqrt
+# print(ans2)
+
+# q4.2
+#
+# number = int(input('enter a number'))
+#
+# print(number**0.5)
+# print(-number**0.5)
+
+# q5
+
+# f = float(input('please enter the temperature in Fahrenheit : '))
+# c = (f-32)*5/9
+# print(c)
+
+# q6
+
+# a = 4
+# b = 2
+# c = -1
+#
+# x1 = (-b + (b ** 2 - 4 * a * c) ** 0.5) / 2 * a
+# x2 = (-b - (b ** 2 - 4 * a * c) ** 0.5) / 2 * a
+# print(round(x1))
+# print(round(x2))
