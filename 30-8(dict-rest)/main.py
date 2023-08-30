cce869a1a20da250c579c18dfb3160f70a42d776
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-08-30 18:04:25 +0300

    json

---
+++ 30-8(dict-rest)/main.py
@@ -0,0 +1,13 @@
+d1 = {'rawad': 90, 'adi': 91, 'haitham': '100'}
+
+print(d1)
+# {'rawad': 90, 'adi': 91, 'haitham': '100'}
+print(type(d1))
+# <class 'dict'>
+print(d1['rawad'])
+# 90
+print(d1['adi'])
+# 91
+d1['adi'] = 100
+print(d1['adi'])
+# 100
